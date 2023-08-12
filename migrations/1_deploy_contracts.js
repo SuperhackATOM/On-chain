98ea7b72a7167f51cbd3f6f8b486e09b4d78d5ac
Cosmodude <vladislav.lenskii@gmail.com>
2023-08-12 16:37:43 +0700

    succesfully deployd with old solidity versions

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,6 +1,6 @@
 
 var Basic = artifacts.require("ERC1155Basic");
-
+Basic.gasMultiplier = 0.9;
 module.exports = function(deployer) {
-  deployer.deploy(Basic);
+  deployer.deploy(Basic,{ gas: 5000000 });
 };
