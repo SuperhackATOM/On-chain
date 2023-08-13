893b0a1d68ca2b1fad5b990fd72597a8e3c3812d
Cosmodude <vladislav.lenskii@gmail.com>
2023-08-13 20:01:49 +0700

    calls for all chains

--- EAS_API.ts
+++ EAS_API.ts
@@ -1,11 +1,11 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
-const client = new ApolloClient({
+const baseClient = new ApolloClient({
     uri: 'https://base-goerli.easscan.org/graphql',
     cache: new InMemoryCache()
 });
 
-client
+baseClient
   .query({
         query: gql`
         query Query($where: AttestationWhereInput) {
@@ -27,4 +27,62 @@ client
           }
         }
     })
-  .then((result) => console.log(result.data));
+  .then((result) => console.log("Atestations on Base", result.data));
+
+const sepoliaClient = new ApolloClient({
+    uri: 'https://sepolia.easscan.org/graphql',
+    cache: new InMemoryCache()
+});
+
+sepoliaClient
+  .query({
+        query: gql`
+        query Query($where: AttestationWhereInput) {
+            attestations(where: $where) {
+            recipient
+            schemaId
+            }
+        }
+        `,
+        variables:
+        {
+            "where": {
+              "attester": {
+                "equals": "0x6f9e2777D267FAe69b0C5A24a402D14DA1fBcaA1"
+              },
+              "schemaId": {
+                "equals": "0x22cc8bd78942a630e45ac11521b9d126675b9e1958c5a269cdbda9c2749dd872"
+              }
+          }
+        }
+    })
+  .then((result) => console.log("Atestations on Sepolia", result.data));
+
+const optimismClient = new ApolloClient({
+    uri: 'https://optimism-goerli-bedrock.easscan.org/graphql',
+    cache: new InMemoryCache()
+});
+
+optimismClient
+  .query({
+        query: gql`
+        query Query($where: AttestationWhereInput) {
+            attestations(where: $where) {
+            recipient
+            schemaId
+            }
+        }
+        `,
+        variables:
+        {
+            "where": {
+              "attester": {
+                "equals": "0x6f9e2777D267FAe69b0C5A24a402D14DA1fBcaA1"
+              },
+              "schemaId": {
+                "equals": "0x22cc8bd78942a630e45ac11521b9d126675b9e1958c5a269cdbda9c2749dd872"
+              }
+          }
+        }
+    })
+  .then((result) => console.log("Atestations on Optimism", result.data));
