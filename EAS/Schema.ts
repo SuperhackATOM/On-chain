522ac4e1f78d3e906c2123888581e815945a7842
Cosmodude <vladislav.lenskii@gmail.com>
2023-08-13 16:42:50 +0700

    schema + packages

---
+++ EAS/Schema.ts
@@ -0,0 +1,15 @@
+export const SCHEMAS = {
+    OP: {
+        ATOM_SCHEMA: "0x22cc8bd78942a630e45ac11521b9d126675b9e1958c5a269cdbda9c2749dd872",
+        CONFIRM_SCHEMA: ""
+    },
+    Base: {
+        ATOM_SCHEMA: "0x22cc8bd78942a630e45ac11521b9d126675b9e1958c5a269cdbda9c2749dd872",
+        CONFIRM_SCHEMA: ""
+    },
+    Sepolia: {
+        ATOM_SCHEMA: "0x22cc8bd78942a630e45ac11521b9d126675b9e1958c5a269cdbda9c2749dd872",
+        CONFIRM_SCHEMA: ""
+    },
+    ATOM_SCHEMA: "0x22cc8bd78942a630e45ac11521b9d126675b9e1958c5a269cdbda9c2749dd872"
+}
